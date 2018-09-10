988f3d5c632f5debb9d79370a377a82680c4be15
Reese Laye <reese.laye@gmail.com>
2018-09-10 16:01:02 +0800

    + 3.2.0 binaries targeting Android

---
+++ 3.2.0/Android/sdk/native/jni/OpenCV-armeabi.mk
@@ -0,0 +1,2 @@
+OPENCV_3RDPARTY_COMPONENTS:=libjpeg libwebp libpng libtiff libjasper IlmImf libprotobuf tegra_hal
+OPENCV_EXTRA_COMPONENTS:=C:/devtools/android-ndk-r14b-windows-x86_64/android-ndk-r14b/platforms/android-9/arch-arm/usr/lib/libz.so dl m log
